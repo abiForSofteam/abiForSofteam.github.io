11ebb539a1e16477e870eb8852b3369766a8e1fa
root <root@aliyou3>
2023-05-14 12:27:59 +0000

    first commit

---
+++ pushcommit.sh
@@ -0,0 +1,14 @@
+# J'ai crée un alias pour executer ce script
+
+# En effet, j'ai exécuté la commande suivante : alias ppp='./sudo gitpushauto.sh'
+
+# Il suffira donc d'exécuter ppp pour lancer le script
+
+# Je pourrais supprimer cet alias en faisant : unalias ppp
+
+# Pour voir les alias déjà en place : alias 
+
+
+sudo git add .
+sudo git commit -m "first commit"
+sudo git push -u origin main
